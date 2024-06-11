f45fbf10e9c571c7442fa9f70f9bc650c5e80292
leela2706 <saileelavardhan12@gmail.com>
2024-06-11 14:39:32 +0530

    add

--- ReactJs/app2-intro/src/Sample2.jsx
+++ ReactJs/app2-intro/src/Sample2.jsx
@@ -11,6 +11,7 @@ export class Sample2 extends Component {
             Message: ''
         },
         allUser: [],
+        showUpdatebtn: false,
     }
     handleChange = (e) => {
         const inputName = e.target.name;
@@ -26,6 +27,7 @@ export class Sample2 extends Component {
         this.setState(newState);
         this.clearForm();
     };
+
     clearForm = () => {
         const newState = { ...this.state };
         newState.userDetails = {
@@ -43,59 +45,86 @@ export class Sample2 extends Component {
         this.setState(newState);
     };
 
-render(){
-    return <div>
-        <h1>Quick Contact</h1>
-        <h4>Contact us today,and get reply with in 24 hours!</h4>
-        <form>
-            <input type="text" name="Name" id="" placeholder="Your Name" value={this.state.userDetails.Name} onChange={this.handleChange}/>
-            <br />
-            <input type="text" name="Email" id="" placeholder="Your Email Address"value={this.state.userDetails.Email} onChange={this.handleChange} />
-            <br />
-            <input type="text" name="Number" id="" placeholder="Your Phone Number" value={this.state.userDetails.Number} onChange={this.handleChange}/>
-            <br />
-            <input type="text" name="WebSite" id="" placeholder="Your Web Site Starts with http://" className="http" value={this.state.userDetails.WebSite} onChange={this.handleChange}/>
-            <br />
-            <input type="text" name="Message" id="" placeholder="Type Your Message Here" className="message"value={this.state.userDetails.Message} onChange={this.handleChange} />
-            <br />
-            <button className="Submit" type="button"onClick={this.addUser}>Submit</button>
+    edituser = (obj) => {
+        const newState = { ...this.state.userDetails }
+        newState.userDetails = obj
+        newState.showUpdatebtn=true;
+        this.setState(newState)
+    }
 
-        </form>
-        <br />
-        <table border={2}>
-            <thead>
-                <tr>
-                    <td>Name</td>
-                    <td>Email</td>
-                    <td>Number</td>
-                    <td>Web Site</td>
-                    <td>Message</td>
-                </tr>
-            </thead>
-            <tbody>
-            {this.state.allUser.map((obj, i) => {
-              return (
-                <tr>
-                  <td>{obj.Name}</td>
-                  <td>{obj.Email}</td>
-                  <td>{obj.Number}</td>
-                  <td>{obj.WebSite}</td>
-                  <td>{obj.Message}</td>
-                  <td>
-                    <button
-                      type="button"
-                      onClick={() => {
-                        this.deleteUser(i);
-                      }}
-                    >
-                      deleteUser
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-    </div>
-}
+    render() {
+        return <div>
+            <h1>Quick Contact</h1>
+            <h4>Contact us today,and get reply with in 24 hours!</h4>
+            <form>
+                <input type="text" name="Name" id="" placeholder="Your Name" value={this.state.userDetails.Name} onChange={this.handleChange} />
+                <br />
+                <input type="text" name="Email" id="" placeholder="Your Email Address" value={this.state.userDetails.Email} onChange={this.handleChange} />
+                <br />
+                <input type="text" name="Number" id="" placeholder="Your Phone Number" value={this.state.userDetails.Number} onChange={this.handleChange} />
+                <br />
+                <input type="text" name="WebSite" id="" placeholder="Your Web Site Starts with http://" className="http" value={this.state.userDetails.WebSite} onChange={this.handleChange} />
+                <br />
+                <input type="text" name="Message" id="" placeholder="Type Your Message Here" className="message" value={this.state.userDetails.Message} onChange={this.handleChange} />
+                <br />
+
+                {this.state.showUpdatebtn ? (
+                    <button type="button" onClick={this.addUser}>updateuser</button>
+                ) : (
+                    <button type="button" onClick={this.addUser}>add user</button>
+                )}
+
+
+
+            </form>
+            <br />
+            <table border={2}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Number</td>
+                        <td>Web Site</td>
+                        <td>Message</td>
+                        <td>edit</td>
+                        <td>delete</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.allUser.map((obj, i) => {
+                        return (
+                            <tr>
+                                <td>{obj.Name}</td>
+                                <td>{obj.Email}</td>
+                                <td>{obj.Number}</td>
+                                <td>{obj.WebSite}</td>
+                                <td>{obj.Message}</td>
+                                <td>
+                                    <button
+                                        type="button"
+                                        onClick={() => {
+                                            this.edituser(obj);
+                                        }}
+                                    >
+                                        edituser
+                                    </button>
+                                </td>
+
+                                <td>
+                                    <button
+                                        type="button"
+                                        onClick={() => {
+                                            this.deleteUser(i);
+                                        }}
+                                    >
+                                        deleteUser
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    }
 }
