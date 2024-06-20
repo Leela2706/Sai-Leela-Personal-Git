dfc266ed2c822aaca88f33ffac020e984fe24441
leela2706 <saileelavardhan12@gmail.com>
2024-06-20 12:29:27 +0530

    added

--- ReactJs/app2-intro/src/App.js
+++ ReactJs/app2-intro/src/App.js
@@ -4,8 +4,9 @@
 // import SimpleForm from "./Did_Mount_Assignments/Sample_1";
 // import { Sample1 } from "./Sample";
 // import { Sample2 } from "./Sample2";
-
 import Assignment_3 from "./BootStrap/Assignment_3";
+import Task_1 from "./Tasks/Task_1";
+import Task_2 from "./Tasks/Task_2";
 
 
 
@@ -18,7 +19,9 @@ function App() {
     {/* <User/> */}
     {/* <Assignment_2/> */}
     {/* <Sample2/> */}
-    <Assignment_3/>
+    {/* <Assignment_3/> */}
+    <Task_1/>
+    <Task_2/>
     
     
 
