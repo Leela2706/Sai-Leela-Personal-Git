82bdfca1ec80e98f4e378ade58c040f29f5d402f
leela2706 <saileelavardhan12@gmail.com>
2024-06-20 12:30:19 +0530

    added

--- ReactJs/app2-intro/src/App.js
+++ ReactJs/app2-intro/src/App.js
@@ -1,9 +1,9 @@
-// import User from "./BootStrap/Assignment_1";
-// import Assignment_2 from "./BootStrap/Assignment2"
-// import Html_Form from "./Did_Mount_Assignments/Html_Form";
-// import SimpleForm from "./Did_Mount_Assignments/Sample_1";
-// import { Sample1 } from "./Sample";
-// import { Sample2 } from "./Sample2";
+import User from "./BootStrap/Assignment_1";
+import Assignment_2 from "./BootStrap/Assignment2"
+import Html_Form from "./Did_Mount_Assignments/Html_Form";
+import SimpleForm from "./Did_Mount_Assignments/Sample_1";
+import { Sample1 } from "./Sample";
+import { Sample2 } from "./Sample2";
 import Assignment_3 from "./BootStrap/Assignment_3";
 import Task_1 from "./Tasks/Task_1";
 import Task_2 from "./Tasks/Task_2";
