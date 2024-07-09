7960b080306afceb1d2a7d60851cff1796387e78
leela2706 <saileelavardhan12@gmail.com>
2024-07-09 11:13:07 +0530

    added

--- ReactJs/app4-context-api/src/App.js
+++ ReactJs/app4-context-api/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Main';
+import HOC_count from './HOC_Components/HOC_Counter';
+import ClickCounter from './HOC_Components/ClickCounter';
+import Hover_Counter from './HOC_Components/Hover_Counter';
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      {/* <Main/>npm  */}
+      <ClickCounter/>
+      <Hover_Counter/>
     </div>
   );
 }
---
+++ ReactJs/app4-context-api/src/HOC_Components/ClickCounter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HOC_count from "./HOC_Counter";
+
+
+function ClickCounter(props) {
+  const { count, incrementCount, decrementCount } = props;
+
+  return (
+    <div>
+      <h1 id="Context_API">Higher Order Component</h1>
+
+      <h1>You clicked {count} times !!!</h1>
+      <button
+        className="btn btn-primary"
+        type="button"
+        onClick={incrementCount}
+      >
+        incrementCount
+      </button>
+      <button className="btn btn-danger" onClick={decrementCount}>
+        decrementCount
+      </button>
+      <hr />
+    </div>
+  );
+}
+
+export default HOC_count(ClickCounter);
