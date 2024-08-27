3670a2761a2c195d6de6d7f79c93924143093edb
leela2706 <saileelavardhan12@gmail.com>
2024-08-27 15:12:21 +0530

    added

--- ReactJs/React_Projects/project_7_pop_up_notification/project_3_pop_up_notification/src/App.js
+++ ReactJs/React_Projects/project_7_pop_up_notification/project_3_pop_up_notification/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import MusicPlayer from './components/MusicPlayer';
-
+import PasswordHide from './components/PassWordHide';
 
 function App() {
   return (
     <div className="App">
-      <MusicPlayer/>
+      <PasswordHide/>
     </div>
   );
 }
