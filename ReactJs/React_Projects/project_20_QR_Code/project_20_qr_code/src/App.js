1f2145256a0c6c1e1ff363ac24707f79208587a1
leela2706 <saileelavardhan12@gmail.com>
2024-08-29 14:57:21 +0530

    added

---
+++ ReactJs/React_Projects/project_20_QR_Code/project_20_qr_code/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import QRCodeGenerator from './components/QR_Code';
+
+function App() {
+  return (
+    <div className="App">
+      <QRCodeGenerator/>
+    </div>
+  );
+}
+
+export default App;
