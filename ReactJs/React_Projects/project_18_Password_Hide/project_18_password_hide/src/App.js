1840c950e77a74e0861e058e65528c7a9a183fb0
leela2706 <saileelavardhan12@gmail.com>
2024-08-29 10:58:17 +0530

    added

---
+++ ReactJs/React_Projects/project_18_Password_Hide/project_18_password_hide/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PasswordHide from './components/Password_Hide';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordHide/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJs/React_Projects/project_19_Password_Validation/project_19_password_validation/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PassWordValidation from './components/Password_Validation';
+
+function App() {
+  return (
+    <div className="App">
+     <PassWordValidation/>
+    </div>
+  );
+}
+
+export default App;
