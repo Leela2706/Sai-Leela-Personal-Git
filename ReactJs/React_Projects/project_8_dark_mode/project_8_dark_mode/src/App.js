d469fafe65e8881a902f93821aa02b5c5165d6f5
leela2706 <saileelavardhan12@gmail.com>
2024-08-29 09:38:39 +0530

    added

---
+++ ReactJs/React_Projects/project_8_dark_mode/project_8_dark_mode/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DarkModeTheme from './components/Dark_mode';
+
+function App() {
+  return (
+    <div className="App">
+      <DarkModeTheme/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJs/React_Projects/project_9_email_subscription/project_9_email_subscription/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EmailSubscription from './components/Email_Subscription';
+
+function App() {
+  return (
+    <div className="App">
+      <EmailSubscription/>
+    </div>
+  );
+}
+
+export default App;
