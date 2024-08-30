f1bdc5950743e855ad4a67108bf5cf22db0ee3bb
leela2706 <saileelavardhan12@gmail.com>
2024-08-30 13:48:33 +0530

    added

---
+++ ReactJs/React_Projects/project_21_Website_Launch/project_21_website_launch/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ComingSoon from './components/Website_Launch';
+
+function App() {
+  return (
+    <div className="App">
+      <ComingSoon/>
+    </div>
+  );
+}
+
+export default App;
