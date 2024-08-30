0e24b1828409056d7fd26b1e5ff6b399226d408e
leela2706 <saileelavardhan12@gmail.com>
2024-08-30 13:41:18 +0530

    added

---
+++ ReactJs/React_Projects/project_22_Mini_Calender/project_22_mini_calender/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MiniCalender from './components/Mini_Calender';
+
+function App() {
+  return (
+    <div className="App">
+      <MiniCalender/>
+    </div>
+  );
+}
+
+export default App;
